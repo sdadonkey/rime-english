c69aa3f344f7e894ef1d8d078e91d74a1366d68c
sdadonkey <onkey@hotmail.com>
2020-09-22 16:59:30 +0800

    The first upload

---
+++ rime.lua
@@ -0,0 +1,6 @@
+local english = require("english")()
+english_processor = english.processor
+english_segmentor = english.segmentor
+english_translator = english.translator
+english_filter = english.filter
+english_filter0 = english.filter0
